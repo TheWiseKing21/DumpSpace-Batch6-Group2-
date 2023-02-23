6187d36606c724aa37692fd74a992f9bf2fa73b3
Jonna077 <jonnamaemayang7@gmail.com>
2023-02-23 15:27:23 +0800

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import "./App.css";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FirebaseState from "./context/FirebaseState";
+import Load from "./components/loading/Load";
+
+const Login = lazy(() => import("./pages/login/Login"));
+const Signup = lazy(() => import("./pages/signup/Signup"));
+// const Home = lazy(() => import("./pages/home/Home"));
+// const Explore = lazy(() => import("./pages/explore/Explore"));
+// const Profile = lazy(() => import("./pages/profile/Profile"));
+const Forgot = lazy(() => import("./pages/forgot/Forgot"));
+const PageNotFound = lazy(() => import("./pages/pagenotfound/PageNotFound"));
 
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <>
+      <FirebaseState>
+        <BrowserRouter>
+          <Suspense fallback={<Load />}>
+            <Routes>
+              {/* <Route path="/" element={<Home />} /> */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              {/* <Route path="/explore" element={<Explore />} /> */}
+              {/* <Route path="/profile/:username" element={<Profile />} /> */}
+              <Route path="/forgot" element={<Forgot />} />
+              <Route exact path="/*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </FirebaseState>
+    </>
   );
 }
 
