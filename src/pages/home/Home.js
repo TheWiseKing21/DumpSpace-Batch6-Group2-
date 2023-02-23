8194f7d983a55a33d5916ad0d37482bc39b7086e
TheWiseKing21 <kings.052100@gmail.com>
2023-02-23 17:04:39 +0800

    added home

---
+++ src/pages/home/Home.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import firebaseContex from "../../context/FirebaseContext";
+import "./Home.css";
+import { RxCross2 } from "react-icons/rx";
+
+const Home = () => {
+  const { posts, allUsers, loading, setIsUpload } = useContext(firebaseContex);
+  const navigate = useNavigate();
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const localUser = JSON.parse(localStorage.getItem("authUser"));
+
+  useEffect(() => {
+    if (localUser === null) {
+      navigate("/login");
+    }
+  }, [localUser]);
+
+  const currentUserInfo = allUsers.filter((val) => {
+    return localUser?.uid === val.id;
+  });
+
+  return <></>;
+};
+
+export default Home;
