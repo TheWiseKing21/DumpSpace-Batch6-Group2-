f7e06fbc4ea989fae7cb3ac8027fbdb3b40c0a44
TheWiseKing21 <kings.052100@gmail.com>
2023-03-01 17:40:25 +0800

    unknow

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -12,7 +12,6 @@ import CustomSnackbar from "../../components/snackbar/snackbar";
 import validator from "validator";
 
 const Signup = () => {
-
   //new const for snackbar
   const [message, setMessage] = useState(false);
   const [showSnackbar, setShowSnackbar] = useState(false);
@@ -52,7 +51,6 @@ const Signup = () => {
     password.length < 8 || email === "" || fullName === "" || username === "";
 
   console.log(invalid);
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -137,154 +135,140 @@ const Signup = () => {
       setShowSnackbar(true);
       setErrorMessage("Is Not Strong Password");
     }
-
   };
 
   return (
-
-    <div className="login-container">
-      <div className="login-wrapper">
-        <div className="login-box">
-        
     <section>
       <div className="signup-container">
-        
         <div className="signup-poster">
-          <img src="/images/logo/signup-poster-1.png" alt="" className="signup-poster-image" />
+          <img
+            src="/images/logo/signup-poster-1.png"
+            alt=""
+            className="signup-poster-image"
+          />
         </div>
-      
-      
-      <div className="signup-wrapper">
-        
-        <div className="signup-box">
-          <div className="logo-wrapper">
-            <img src="/images/logo/dump-space-logo.png" alt="" className="" />
+
+        <div className="signup-wrapper">
+          <div className="signup-box">
+            <div className="logo-wrapper">
+              <img src="/images/logo/dump-space-logo.png" alt="" className="" />
+            </div>
+
+            {!isEmailSend ? (
+              <div className="login-form-wrapper">
+                <form className="login-form" onSubmit={handleSubmit}>
+                  <div className="input-label">
+                    <input
+                      type="email"
+                      placeholder="Email address"
+                      aria-label="Enter your email address"
+                      aria-required="true"
+                      autoComplete="off"
+                      name="email"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="input-label">
+                    <input
+                      type="text"
+                      placeholder="FullName"
+                      aria-label="Enter your full name"
+                      aria-required="true"
+                      autoComplete="off"
+                      name="fullName"
+                      required
+                      onChange={(e) => setFullName(e.target.value)}
+                    />
+                  </div>
+                  <div className="input-label">
+                    <input
+                      type="text"
+                      placeholder="Username"
+                      aria-label="Enter your username"
+                      aria-required="true"
+                      autoComplete="off"
+                      name="username"
+                      required
+                      onChange={(e) => setUsername(e.target.value)}
+                      onKeyDown={(e) =>
+                        e.code === "Space" && e.preventDefault()
+                      }
+                    />
+                  </div>
+                  <div className="input-label">
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      aria-label="Enter your password"
+                      aria-required="true"
+                      autoComplete="off"
+                      name="password"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="button-wrapper ">
+                    <button
+                      disabled={invalid}
+                      type="submit"
+                      className="pushable"
+                      style={{
+                        opacity: (invalid || loading) && "0.5",
+                      }}
+                    >
+                      <span class="front">
+                        {/* Start dumping... */}
+                        Sign Up
+                      </span>
+                    </button>
+                    {/* {loading && <Loading />} */}
+                  </div>
+                </form>
+                {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+
+                <div className="redirect-box">
+                  <div className="redirect-text">
+                    <p>
+                      Have an account?{" "}
+                      <Link to="/login" className="cur-point">
+                        Log In
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              // email send confirmation
+
+              <>
+                <CustomSnackbar
+                  open={showSnackbar}
+                  message="Sign up successful"
+                  variant="success"
+                  onClose={handleSnackbarClose}
+                />
+
+                <div className="signup-confirm-email-wrapper">
+                  <div className="confirm-email-image-wrapper">
+                    <img
+                      // src="/images/confirm-email.svg"
+                      src="/images/logo/confirm-img.png"
+                      alt="confirm-email"
+                      className="confirm-email-image"
+                    />
+                  </div>
+                  <div className="confirm-email-message">
+                    Verification link send to your email (check inbox or spam
+                    folder). Please verify email first...
+                  </div>
+                </div>
+              </>
+            )}
           </div>
 
-
-          {!isEmailSend ? (
-            
-            <div className="login-form-wrapper">
-              <form className="login-form" onSubmit={handleSubmit}>
-                <div className="input-label">
-                  <input
-                    type="email"
-                    placeholder="Email address"
-                    aria-label="Enter your email address"
-                    aria-required="true"
-                    autoComplete="off"
-                    name="email"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="input-label">
-                  <input
-                    type="text"
-                    placeholder="FullName"
-                    aria-label="Enter your full name"
-                    aria-required="true"
-                    autoComplete="off"
-                    name="fullName"
-                    required
-                    onChange={(e) => setFullName(e.target.value)}
-                  />
-                </div>
-                <div className="input-label">
-                  <input
-                    type="text"
-                    placeholder="Username"
-                    aria-label="Enter your username"
-                    aria-required="true"
-                    autoComplete="off"
-                    name="username"
-                    required
-                    onChange={(e) => setUsername(e.target.value)}
-                    onKeyDown={(e) => e.code === "Space" && e.preventDefault()}
-                  />
-                </div>
-                <div className="input-label">
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    aria-label="Enter your password"
-                    aria-required="true"
-                    autoComplete="off"
-                    name="password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-
-                <div className="button-wrapper ">
-                  <button
-                    disabled={invalid}
-                    type="submit"
-                    className="pushable"
-                    style={{
-                      opacity: (invalid || loading) && "0.5",
-                    }}
-                  >
-                    <span class="front">
-                      {/* Start dumping... */}
-                      Sign Up
-                    </span>
-                  </button>
-                  {loading && <Loading />}
-                  <CustomSnackbar
-                    open={showSnackbar}
-                    message={message}
-                    variant="success"
-                    onClose={handleSnackbarClose}
-                  />
-                </div>
-              </form>
-              {errorMessage && <p className="errorMessage">{errorMessage}</p>}
-              
-              <div className="redirect-box">
-                <div className="redirect-text">
-                  <p>
-                    Have an account?{" "}
-                    <Link to="/login" className="cur-point">
-                      Log In
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            </div>
-          ) : (
-            // email send confirmation
-
-            <>
-              <CustomSnackbar
-                open={showSnackbar}
-                message="Sign up successful"
-                variant="success"
-                onClose={handleSnackbarClose}
-              />
-              
-
-              <div className="signup-confirm-email-wrapper">
-                <div className="confirm-email-image-wrapper">
-                  <img
-                    // src="/images/confirm-email.svg"
-                    src = "/images/logo/confirm-img.png"
-                    alt="confirm-email"
-                    className="confirm-email-image"
-                  />
-                </div>
-                <div className="confirm-email-message">
-
-                  Please make sure your email is verified.
-                  Verification link will be sent to your email, please check inbox or spam
-                  folder.
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-        
-        {/* <div className="redirect-box login-box">
+          {/* <div className="redirect-box login-box">
           <div className="redirect-text">
             <p>
               Have an account?{" "}
@@ -294,11 +278,10 @@ const Signup = () => {
             </p>
           </div>
         </div> */}
+        </div>
+        {/* <Footer /> */}
       </div>
-      {/* <Footer /> */}
-    </div>
     </section>
-
   );
 };
 
