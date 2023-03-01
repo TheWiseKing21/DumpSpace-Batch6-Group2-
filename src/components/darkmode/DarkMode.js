59270ee790ac271beaa539f621d255d53c1a1d0f
TheWiseKing21 <kings.052100@gmail.com>
2023-03-01 17:33:45 +0800

    dark mode saved in firebase

--- src/components/darkmode/DarkMode.js
+++ src/components/darkmode/DarkMode.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ReactComponent as Sun } from "../../assets/img/Sun.svg";
 import { ReactComponent as Moon } from "../../assets/img/Moon.svg";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import "./DarkMode.css";
+import { auth, db } from "../../config/FirebaseConfig";
+import firebaseContex from "../../context/FirebaseContext";
+import { async } from "@firebase/util";
 
-const DarkMode = ({ setDarkMode }) => {
+const DarkMode = ({ darkMode }) => {
+  const [toggle, setToggle] = useState(false);
+  useEffect(() => {
+    if (auth.currentUser !== null) {
+      getDefaultTheme();
+      if (toggle) {
+        setDarkTheme();
+      } else {
+        setLightTheme();
+      }
+    }
+  });
+
   const setDarkTheme = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
-    setDarkMode(true);
   };
 
   const setLightTheme = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
-    setDarkMode(false);
+  };
+
+  const getDefaultTheme = async () => {
+    const docRef = doc(db, "userinfo", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    setToggle(docSnap.data().isDark);
+  };
+
+  const setDefaultTheme = async (darkState) => {
+    await updateDoc(doc(db, "userinfo", auth.currentUser.uid), {
+      isDark: darkState,
+    });
   };
 
   const toggleTheme = (e) => {
-    if (e.target.checked) setDarkTheme();
-    else setLightTheme();
+    if (e.target.checked) {
+      setDefaultTheme(e.target.checked);
+    } else {
+      setDefaultTheme(e.target.checked);
+    }
   };
   return (
     <div className="dark_mode">
@@ -24,6 +53,7 @@ const DarkMode = ({ setDarkMode }) => {
         className="dark_mode_input"
         type="checkbox"
         id="darkmode-toggle"
+        checked={toggle}
         onChange={toggleTheme}
       />
       <label className="dark_mode_label" htmlFor="darkmode-toggle">
